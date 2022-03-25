e02533184abfe685d206e592fc2dea9207f587b9
sp381 <sarah.powell381@gmail.com>
2022-03-24 20:35:35 -0700

    added a question to prompt

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+const inquirer = require('inquirer'); 
+const fs = require('fs'); 
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt([
+      {
+       type: 'input',
+       name: 'Project-Title',
+       message: 'Enter a title for your project: '
+      }
+      
+    ])
+    .then((answers) => {
+      console.log(answers);
+      fs.writeFile('message.txt', 'data', (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
 
 // Function call to initialize app
 init();
